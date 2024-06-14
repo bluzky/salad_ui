04d69b508b8f35cf065fbbce0034ecdb6f68ec17
Kamaro <kamaro.lambert@amitruck.com>
2024-06-14 23:33:08 +0300

    Formatted codes approprietely

--- test/salad_ui/dropdown_menu_test.exs
+++ test/salad_ui/dropdown_menu_test.exs
@@ -32,29 +32,29 @@ defmodule SaladUI.DropdownMenuTest do
         <.dropdown_menu_content>
           <.menu class="w-56">
             <.menu_label>Account</.menu_label>
-             <.menu_separator />
+            <.menu_separator />
             <.menu_group>
               <.menu_item>
                 <.icon name="hero-user" class="mr-2 h-4 w-4" /> <span>Profile</span>
                 <.menu_shortcut>⌘P</.menu_shortcut>
               </.menu_item>
-              
+
               <.menu_item>
                 <.icon name="hero-banknotes" class="mr-2 h-4 w-4" /> <span>Billing</span>
                 <.menu_shortcut>⌘B</.menu_shortcut>
               </.menu_item>
-              
+
               <.menu_item>
                 <.icon name="hero-cog-6-tooth" class="mr-2 h-4 w-4" /> <span>Settings</span>
                 <.menu_shortcut>⌘S</.menu_shortcut>
               </.menu_item>
             </.menu_group>
-             <.menu_separator />
+            <.menu_separator />
             <.menu_group>
               <.menu_item>
                 <.icon name="hero-users" class="mr-2 h-4 w-4" /> <span>Team</span>
               </.menu_item>
-              
+
               <.menu_item disabled>
                 <.icon name="hero-plus" class="mr-2 h-4 w-4" /> <span>New team</span>
                 <.menu_shortcut>⌘T</.menu_shortcut>
@@ -80,33 +80,33 @@ defmodule SaladUI.DropdownMenuTest do
             <.dropdown_menu_trigger>
               <.button variant="outline">Click me</.button>
             </.dropdown_menu_trigger>
-            
+
             <.dropdown_menu_content>
               <.menu class="w-56">
                 <.menu_label>Account</.menu_label>
-                 <.menu_separator />
+                <.menu_separator />
                 <.menu_group>
                   <.menu_item>
                     <.icon name="hero-user" class="mr-2 h-4 w-4" /> <span>Profile</span>
                     <.menu_shortcut>⌘P</.menu_shortcut>
                   </.menu_item>
-                  
+
                   <.menu_item>
                     <.icon name="hero-banknotes" class="mr-2 h-4 w-4" /> <span>Billing</span>
                     <.menu_shortcut>⌘B</.menu_shortcut>
                   </.menu_item>
-                  
+
                   <.menu_item>
                     <.icon name="hero-cog-6-tooth" class="mr-2 h-4 w-4" /> <span>Settings</span>
                     <.menu_shortcut>⌘S</.menu_shortcut>
                   </.menu_item>
                 </.menu_group>
-                 <.menu_separator />
+                <.menu_separator />
                 <.menu_group>
                   <.menu_item>
                     <.icon name="hero-users" class="mr-2 h-4 w-4" /> <span>Team</span>
                   </.menu_item>
-                  
+
                   <.menu_item disabled>
                     <.icon name="hero-plus" class="mr-2 h-4 w-4" /> <span>New team</span>
                     <.menu_shortcut>⌘T</.menu_shortcut>
