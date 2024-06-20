54a6888a59953a6fce7125350a73f37d06c9335a
Kamaro <kamaro.lambert@amitruck.com>
2024-06-20 20:09:55 +0300

    Added form tests

--- test/salad_ui/dropdown_menu_test.exs
+++ test/salad_ui/dropdown_menu_test.exs
@@ -1,24 +1,24 @@
 defmodule SaladUI.DropdownMenuTest do
   use ComponentCase
 
-  import SaladUI.DropdownMenu
   import SaladUI.Button
-  import SaladUI.Menu
+  import SaladUI.DropdownMenu
   import SaladUI.Icon
+  import SaladUI.Menu
 
   describe "Test Dropdown menu" do
     test "It renders dropdown trigger correctly" do
       assigns = %{}
 
-      html = rendered_to_string(
-        ~H"""
+      html =
+        rendered_to_string(~H"""
         <.dropdown_menu_trigger>
           <.button variant="outline">Click me</.button>
         </.dropdown_menu_trigger>
-        """
-      )
+        """)
 
       assert html =~ "Click me"
+
       for css_class <- ~w(dropdown-menu-trigger peer) do
         assert html =~ css_class
       end
@@ -26,47 +26,48 @@ defmodule SaladUI.DropdownMenuTest do
 
     test "It renders dropdown menu content correctly" do
       assigns = %{}
-      html = rendered_to_string(
-        ~H"""
-          <.dropdown_menu_content>
-            <.menu class="w-56">
-              <.menu_label>Account</.menu_label>
-              <.menu_separator />
-              <.menu_group>
-                <.menu_item>
-                  <.icon name="hero-user" class="mr-2 h-4 w-4" />
-                  <span>Profile</span>
-                  <.menu_shortcut>⌘P</.menu_shortcut>
-                </.menu_item>
-                <.menu_item>
-                  <.icon name="hero-banknotes" class="mr-2 h-4 w-4" />
-                  <span>Billing</span>
-                  <.menu_shortcut>⌘B</.menu_shortcut>
-                </.menu_item>
-                <.menu_item>
-                  <.icon name="hero-cog-6-tooth" class="mr-2 h-4 w-4" />
-                  <span>Settings</span>
-                  <.menu_shortcut>⌘S</.menu_shortcut>
-                </.menu_item>
-              </.menu_group>
-              <.menu_separator />
-              <.menu_group>
-                <.menu_item>
-                  <.icon name="hero-users" class="mr-2 h-4 w-4" />
-                  <span>Team</span>
-                </.menu_item>
-                <.menu_item disabled>
-                  <.icon name="hero-plus" class="mr-2 h-4 w-4" />
-                  <span>New team</span>
-                  <.menu_shortcut>⌘T</.menu_shortcut>
-                </.menu_item>
-              </.menu_group>
-            </.menu>
-        </.dropdown_menu_content>
-        """
-      )
 
-      for css_class <- ~w(z-50 animate-in peer-data-[state=closed]:fade-out-0 peer-data-[state=open]:fade-in-0 peer-data-[state=closed]:zoom-out-95 peer-data-[state=open]:zoom-in-95 peer-data-[side=bottom]:slide-in-from-top-2 peer-data-[side=left]:slide-in-from-right-2 peer-data-[side=right]:slide-in-from-left-2 peer-data-[side=top]:slide-in-from-bottom-2) do
+      html =
+        rendered_to_string(~H"""
+        <.dropdown_menu_content>
+          <.menu class="w-56">
+            <.menu_label>Account</.menu_label>
+            <.menu_separator />
+            <.menu_group>
+              <.menu_item>
+                <.icon name="hero-user" class="mr-2 h-4 w-4" />
+                <span>Profile</span>
+                <.menu_shortcut>⌘P</.menu_shortcut>
+              </.menu_item>
+              <.menu_item>
+                <.icon name="hero-banknotes" class="mr-2 h-4 w-4" />
+                <span>Billing</span>
+                <.menu_shortcut>⌘B</.menu_shortcut>
+              </.menu_item>
+              <.menu_item>
+                <.icon name="hero-cog-6-tooth" class="mr-2 h-4 w-4" />
+                <span>Settings</span>
+                <.menu_shortcut>⌘S</.menu_shortcut>
+              </.menu_item>
+            </.menu_group>
+            <.menu_separator />
+            <.menu_group>
+              <.menu_item>
+                <.icon name="hero-users" class="mr-2 h-4 w-4" />
+                <span>Team</span>
+              </.menu_item>
+              <.menu_item disabled>
+                <.icon name="hero-plus" class="mr-2 h-4 w-4" />
+                <span>New team</span>
+                <.menu_shortcut>⌘T</.menu_shortcut>
+              </.menu_item>
+            </.menu_group>
+          </.menu>
+        </.dropdown_menu_content>
+        """)
+
+      for css_class <-
+            ~w(z-50 animate-in peer-data-[state=closed]:fade-out-0 peer-data-[state=open]:fade-in-0 peer-data-[state=closed]:zoom-out-95 peer-data-[state=open]:zoom-in-95 peer-data-[side=bottom]:slide-in-from-top-2 peer-data-[side=left]:slide-in-from-right-2 peer-data-[side=right]:slide-in-from-left-2 peer-data-[side=top]:slide-in-from-bottom-2) do
         assert html =~ css_class
       end
     end
@@ -74,14 +75,14 @@ defmodule SaladUI.DropdownMenuTest do
     test "It renderes dropdown menu correctly" do
       assigns = %{}
 
-      html = rendered_to_string(
-        ~H"""
-          <div class="mt-24">
+      html =
+        rendered_to_string(~H"""
+        <div class="mt-24">
           <.dropdown_menu>
-                <.dropdown_menu_trigger>
-                  <.button variant="outline">Click me</.button>
-                </.dropdown_menu_trigger>
-          <.dropdown_menu_content>
+            <.dropdown_menu_trigger>
+              <.button variant="outline">Click me</.button>
+            </.dropdown_menu_trigger>
+            <.dropdown_menu_content>
               <.menu class="w-56">
                 <.menu_label>Account</.menu_label>
                 <.menu_separator />
@@ -115,11 +116,10 @@ defmodule SaladUI.DropdownMenuTest do
                   </.menu_item>
                 </.menu_group>
               </.menu>
-          </.dropdown_menu_content>
+            </.dropdown_menu_content>
           </.dropdown_menu>
-          </div>
-        """
-      )
+        </div>
+        """)
 
       assert html =~ "<span>Profile</span>"
       assert html =~ "<span>Billing</span>"
@@ -128,6 +128,7 @@ defmodule SaladUI.DropdownMenuTest do
       assert html =~ "⌘T"
       assert html =~ "⌘S"
       assert html =~ "hero-users"
+
       for css_class <- ~w(relative group inline-block) do
         assert html =~ css_class
       end
---
+++ test/salad_ui/form_test.exs
@@ -0,0 +1,124 @@
+defmodule SaladUI.FormTest do
+  use ComponentCase
+
+  import SaladUI.Button
+  import SaladUI.Form
+  import SaladUI.Input
+
+  describe "Test form components" do
+    test "It renders form item correctly" do
+      assigns = %{}
+
+      html =
+        ~H"""
+        <.form_item>We are inside form item</.form_item>
+        """
+        |> rendered_to_string()
+        |> clean_string()
+
+      assert html =~ "<div class=\"space-y-2\">We are inside form item</div>"
+    end
+
+    test "It renders form label_correctly" do
+      assigns = %{}
+
+      html =
+        ~H"""
+        <.form_label>This is a label</.form_label>
+        """
+        |> rendered_to_string()
+        |> clean_string()
+
+      assert html =~
+               "<label class=\"font-medium leading-none text-sm false peer-disabled:cursor-not-allowed peer-disabled:opacity-70\">This is a label</label>"
+    end
+
+    test "It renders form_control" do
+      assigns = %{}
+
+      html =
+        ~H"""
+        <.form_control>Form control</.form_control>
+        """
+        |> rendered_to_string()
+        |> clean_string()
+
+      assert html =~ "Form control"
+    end
+
+    test "It renders form_description correctly" do
+      assigns = %{}
+
+      html =
+        ~H"""
+        <.form_description>This is a form description</.form_description>
+        """
+        |> rendered_to_string()
+        |> clean_string()
+
+      assert html == "<p class=\"text-muted-foreground text-sm\">This is a form description</p>"
+    end
+
+    test "It renders form message correctly" do
+      assigns = %{}
+
+      html =
+        ~H"""
+        <.form_message>This is a form message</.form_message>
+        """
+        |> rendered_to_string()
+        |> clean_string()
+
+      assert html =~ "<p class=\"text-destructive font-medium text-sm\">This is a form message</p>"
+    end
+
+    test "It returns the message from a tuple {}" do
+      assert translate_error({"Error Message", []}) == "Error Message"
+    end
+
+    test "It renders an entire form correctly" do
+      assigns = %{form: %{}, myself: "test-string"}
+
+      html =
+        ~H"""
+        <.form
+          class="space-y-6"
+          for={@form}
+          id="project-form"
+          phx-target={@myself}
+          phx-change="validate"
+          phx-submit="save"
+        >
+          <.form_item>
+            <.form_label>What is your project's name?</.form_label>
+            
+            <.form_control>
+              <.input field={@form[:name]} type="text" phx-debounce="500" />
+            </.form_control>
+            
+            <.form_description>This is your public display name.</.form_description>
+             <.form_message field={@form[:name]} />
+          </.form_item>
+          
+          <div class="w-full flex flex-row-reverse">
+            <.button class="btn btn-secondary btn-md" icon="inbox_arrow_down" phx-disable-with="Saving...">
+              Save project
+            </.button>
+          </div>
+        </.form>
+        """
+        |> rendered_to_string()
+        |> clean_string()
+
+      assert html =~
+               "<form id=\"project-form\" class=\"space-y-6\" phx-submit=\"save\" phx-change=\"validate\" phx-target=\"test-string\">"
+
+      assert html =~
+               "<label class=\"font-medium leading-none text-sm false peer-disabled:cursor-not-allowed peer-disabled:opacity-70\">\What is your project's name?</label>"
+
+      assert html =~ "<p class=\"text-muted-foreground text-sm\">This is your public display name.</p>"
+      assert html =~ "Save project</button>"
+      assert html =~ "</div></form>"
+    end
+  end
+end
