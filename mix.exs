e183e182cc67e580a42ba6321b3c85521eff2601
Dzung Nguyen <bluesky.1289@gmail.com>
2024-11-06 09:30:40 +0700

    Bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule SaladUI.MixProject do
   def project do
     [
       app: :salad_ui,
-      version: "0.12.0",
+      version: "0.13.0",
       elixir: "~> 1.14",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
