aa0b03dd4a417444523913cdfbee5b9a98208b9d
Dzung Nguyen <bluesky.1289@gmail.com>
2024-11-07 21:55:12 +0700

    Fix white checked checkbox when hover

--- lib/salad_ui/checkbox.ex
+++ lib/salad_ui/checkbox.ex
@@ -28,7 +28,7 @@ defmodule SaladUI.Checkbox do
       type="checkbox"
       class={
         classes([
-          "peer h-4 w-4 shrink-0 rounded-sm border border-primary shadow focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 checked:bg-primary checked:text-primary-foreground",
+          "peer h-4 w-4 shrink-0 rounded-sm border border-primary shadow focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 checked:bg-primary checked:focus:bg-primary checked:hover:bg-primary checked:text-primary-foreground",
           @class
         ])
       }
