0e7360dd48976619d3de57742ec929ec6acf3152
selenil <abergesdesouza@gmail.com>
2025-01-20 12:42:15 -0500

    integrate zag with slider

--- lib/salad_ui/slider.ex
+++ lib/salad_ui/slider.ex
@@ -12,15 +12,30 @@ defmodule SaladUI.Slider do
 
   """
   attr :id, :string, required: true
-  attr :class, :string, default: nil
   attr :name, :string, default: nil
-  attr :value, :integer, default: 0, doc: ""
-  attr :"default-value", :integer
-  attr :field, Phoenix.HTML.FormField, doc: "a form field struct retrieved from the form, for example: @form[:email]"
-
+  attr :value, :integer, default: 0
   attr :min, :integer, default: 0
   attr :max, :integer, default: 100
   attr :step, :integer, default: 1
+
+  attr :field, Phoenix.HTML.FormField, doc: "a form field struct retrieved from the form, for example: @form[:email]"
+
+  attr :options, :map,
+    default: %{},
+    doc: """
+    Options supported by the Zag.js library for configuring this component.
+    See https://zagjs.com/components/react/slider#machine-context for full list of available options.
+    """
+
+  attr :listeners, :list,
+    default: [],
+    doc: """
+    Event listeners supported by the Zag.js library for configuring this component.
+    Each listener it's a tuple with the event name as an atom and where to handle it (`:client`, `:server` or both).
+    See https://zagjs.com/components/react/slider#listening-for-events for full list of available listeners.
+    """
+
+  attr :class, :string, default: nil
   attr :rest, :global
 
   def slider(assigns) do
@@ -37,40 +52,40 @@ defmodule SaladUI.Slider do
     ~H"""
     <div
       class={classes(["relative w-full", @class])}
-      style={"--#{@id}-val: #{(@value - @min)/(@max - @min) * 100}"}
+      id={@id}
+      phx-hook="ZagHook"
+      data-component="slider"
+      data-parts={Jason.encode!(["control", "track", "range", "thumb", "hidden-input"])}
+      data-options={
+        %{
+          # zag expects the value as an array
+          value: [@value],
+          min: @min,
+          max: @max,
+          step: @step,
+          name: @name
+        }
+        |> Map.merge(@options)
+        |> Jason.encode!()
+      }
+      data-listeners={@listeners |> tuples_to_lists() |> Jason.encode!()}
     >
-      <span class={["relative flex w-full touch-none select-none items-center"]}>
-        <span
-          data-orientation="horizontal"
-          class="relative h-2 w-full grow overflow-hidden rounded-full bg-secondary"
-        >
-          <span
-            data-orientation="horizontal"
-            class="absolute h-full bg-primary"
-            style={"left: 0%; right: calc(100% - var(--#{@id}-val)*1%)"}
-          >
-          </span>
+      <span data-part="control" class="flex w-full items-center">
+        <span data-part="track" class="h-2 w-full grow overflow-hidden rounded-full bg-secondary">
+          <span data-part="range" class="h-full bg-primary"></span>
         </span>
-        <span style={"transform: translateX(-50%); position: absolute; left: calc(var(--#{@id}-val)*1%);"}>
-          <span
-            role="slider"
-            aria-valuemin={@min}
-            aria-valuemax={@max}
-            aria-orientation="horizontal"
-            data-orientation="horizontal"
-            tabindex="0"
-            class="block h-5 w-5 rounded-full border-2 border-primary bg-background ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50"
-          >
-          </span>
+        <span
+          data-part="thumb"
+          data-options={Jason.encode!(%{value: @value})}
+          class="h-5 w-5 rounded-full border-2 border-primary bg-background ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50"
+        >
         </span>
       </span>
       <input
-        type="range"
-        class="absolute top-0 -left-2 z-1 w-full appearance-none cursor-pointer opacity-0"
+        data-part="hidden-input"
+        data-options={Jason.encode!(%{value: @value})}
+        class="w-full cursor-pointer"
         phx-update="ignore"
-        style="width: calc(100% + 20px)"
-        oninput={"this.parentNode.style='--#{@id}-val:' + (this.value - #{@min})/#{@max - @min}*100; return true;"}
-        {%{min: @min, max: @max, value: @value, step: @step, id: @id, name: @name}}
         {@rest}
       />
     </div>
