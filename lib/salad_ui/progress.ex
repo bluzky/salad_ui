0ebe6edf3569c4a636849504ecc6f6843d342bf1
selenil <abergesdesouza@gmail.com>
2025-01-23 08:34:32 -0500

    integrate zag with progress component

--- lib/salad_ui/progress.ex
+++ lib/salad_ui/progress.ex
@@ -11,8 +11,19 @@ defmodule SaladUI.Progress do
       <.progress class="w-[60%]" value={20}/>
 
   """
+  attr :id, :string, required: true
+  attr :value, :integer, default: 0
+  attr :min, :integer, default: 0
+  attr :max, :integer, default: 100
+
+  attr :options, :map,
+    default: %{},
+    doc: """
+    Options supported by the Zag.js library for configuring this component.
+    See https://zagjs.com/components/react/linear-progress#machine-context for full list of available options.
+    """
+
   attr :class, :string, default: nil
-  attr :value, :integer, default: 0, doc: ""
   attr :rest, :global
 
   def progress(assigns) do
@@ -21,12 +32,23 @@ defmodule SaladUI.Progress do
     ~H"""
     <div
       class={classes(["relative h-4 w-full overflow-hidden rounded-full bg-secondary", @class])}
+      id={@id}
+      phx-hook="ZagHook"
+      data-component="progress"
+      data-parts={Jason.encode!(["track", "range"])}
+      data-options={
+        %{
+          value: @value,
+          min: @min,
+          max: @max
+        }
+        |> Map.merge(@options)
+        |> Jason.encode!()
+      }
       {@rest}
     >
-      <div
-        class="h-full w-full flex-1 bg-primary transition-all"
-        style={"transform: translateX(-#{100 - (@value || 0)}%)"}
-      >
+      <div data-part="track">
+        <div data-part="range" class="bg-primary transition-all" />
       </div>
     </div>
     """
