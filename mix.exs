1fb404637c872501ae781505160a3bc51031e5f5
Dzung Nguyen <bluesky.1289@gmail.com>
2025-03-18 08:47:13 +0700

    Release v0.14.9

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule SaladUI.MixProject do
   def project do
     [
       app: :salad_ui,
-      version: "0.14.8",
+      version: "0.14.9",
       elixir: "~> 1.14",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
