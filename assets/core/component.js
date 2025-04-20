da7244489e8ead4d685feba6e4d98e515e517edd
Ilya Bayaliyev <ilyabayaliyev@gmail.com>
2025-04-20 16:35:47 +0500

    remove unnecesary changes

--- assets/core/component.js
+++ assets/core/component.js
@@ -34,10 +34,9 @@ class Component {
     this.allParts = Array.from(this.el.querySelectorAll("[data-part]")).concat([
       this.el,
     ]);
-
     if (ignoreItems) {
       this.allParts = this.allParts.filter(
-        (element) => !element.dataset?.part?.startsWith("item"),
+        (element) => !element.dataset.part.startsWith("item"),
       );
     }
 
