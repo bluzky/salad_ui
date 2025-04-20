c2a322173e84f644b7bde1d3c67d3e8718ad79b5
Ilya Bayaliyev <ilyabayaliyev@gmail.com>
2025-04-20 16:39:43 +0500

    copy close icon from shadcn

--- lib/salad_ui/dialog.ex
+++ lib/salad_ui/dialog.ex
@@ -137,11 +137,15 @@ defmodule SaladUI.Dialog do
         >
           <svg
             xmlns="http://www.w3.org/2000/svg"
-            fill="none"
+            width="24"
+            height="24"
             viewBox="0 0 24 24"
-            stroke-width="1.5"
+            fill="none"
             stroke="currentColor"
-            class="w-5 h-5"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            class="lucide lucide-x h-4 w-4"
           >
             <path d="M18 6 6 18"></path>
             <path d="m6 6 12 12"></path>
