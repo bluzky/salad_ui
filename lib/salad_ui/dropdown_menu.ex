efd558cdfd936ab6a6f583f75b1f853c6070a00a
Dung Nguyen <bluesky.1289@gmail.com>
2025-04-24 09:50:57 +0700

    update disable attribute

--- lib/salad_ui/dropdown_menu.ex
+++ lib/salad_ui/dropdown_menu.ex
@@ -245,7 +245,7 @@ defmodule SaladUI.DropdownMenu do
     ~H"""
     <div
       data-part="item"
-      data-disabled={to_string(@disabled)}
+      data-disabled={@disabled}
       data-variant={@variant}
       class={
         classes([
@@ -281,7 +281,7 @@ defmodule SaladUI.DropdownMenu do
     ~H"""
     <div
       data-part="checkbox-item"
-      data-disabled={to_string(@disabled)}
+      data-disabled={@disabled}
       class={
         classes([
           "focus:bg-accent focus:text-accent-foreground relative flex cursor-default items-center gap-2 rounded-sm py-1.5 pr-2 pl-8 text-sm outline-hidden select-none data-[disabled]:pointer-events-none data-[disabled]:opacity-50 [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-4",
@@ -332,7 +332,7 @@ defmodule SaladUI.DropdownMenu do
     ~H"""
     <div
       data-part="radio-item"
-      data-disabled={to_string(@disabled)}
+      data-disabled={@disabled}
       class={
         classes([
           "focus:bg-accent focus:text-accent-foreground relative flex cursor-default items-center gap-2 rounded-sm py-1.5 pr-2 pl-8 text-sm outline-hidden select-none data-[disabled]:pointer-events-none data-[disabled]:opacity-50 [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-4",
@@ -481,7 +481,7 @@ defmodule SaladUI.DropdownMenu do
     ~H"""
     <div
       data-part="sub-trigger"
-      data-disabled={to_string(@disabled)}
+      data-disabled={@disabled}
       class={
         classes([
           "focus:bg-accent focus:text-accent-foreground data-[state=open]:bg-accent data-[state=open]:text-accent-foreground flex cursor-default items-center rounded-sm px-2 py-1.5 text-sm outline-hidden select-none data-[inset]:pl-8",
