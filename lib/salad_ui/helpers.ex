c54953cc12630fca409cd57d65b85a69a38ba001
Mitja Gorisek <mik.gorisek@gmail.com>
2025-06-24 09:25:36 +0200

    Change button lg size to have 12px (XL) radius.

--- lib/salad_ui/helpers.ex
+++ lib/salad_ui/helpers.ex
@@ -104,7 +104,7 @@ defmodule SaladUI.Helpers do
     size: %{
       "default" => "h-9 px-4 py-2",
       "sm" => "h-8 rounded-md px-3 text-xs",
-      "lg" => "h-10 rounded-md px-8",
+      "lg" => "h-10 rounded-xl px-8",
       "icon" => "h-9 w-9"
     }
   }
